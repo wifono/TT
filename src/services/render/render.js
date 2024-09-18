8d10c66dc25cb6a5352cbf880fd8f8c2d5064bed
Daniel Trstansky <daniel.trstansky@nov.sk>
2024-09-18 08:37:44 +0200

    fix

--- src/services/render/render.js
+++ src/services/render/render.js
@@ -43,92 +43,49 @@ export const render = (app) => {
     'Content-Disposition': 'inline; filename="image.png"'
   }
 
-  app.post('/upload-folder', upload.array('files'), (req, res) => {
+  app.post('/upload-template', upload.single('file'), async (req, res) => {
     try {
-      const template = req.body.template
-      const customer = req.query.customer
-      const paths = req.body.paths
-      const commonBasePath = path.dirname(paths[0])
-      console.log('File Paths:', paths)
+      const templateFile = req.file
+      console.log(templateFile)
 
-      if (!paths || paths.length === 0) {
-        return res.status(400).json({ message: 'No files provided' })
+      if (!templateFile) {
+        throw new Error('No template file uploaded')
       }
 
-      const baseDirectory = path.join('uploads', customer, template)
+      // Clear the uploads directory
+      try {
+        const files = fs.readdir('uploads')
+        for (const file of files) {
+          const filePath = path.join('uploads', file)
+          console.log(filePath)
 
-      if (!fs.existsSync(baseDirectory)) {
-        fs.mkdirSync(baseDirectory, { recursive: true })
-        console.log(`Created directory: ${baseDirectory}`)
-      }
-
-      paths.forEach((filePath) => {
-        const fileName = path.basename(filePath)
-
-        const templateRoot = path.join('C:', 'Users', 'nov', 'Documents', 'james-templates')
-        const relativeFilePath = path.relative(templateRoot, filePath)
-        const destinationPath = path.join('uploads', relativeFilePath)
-        console.log('Destination Path:', destinationPath)
-
-        const destinationDir = path.dirname(destinationPath)
-        if (!fs.existsSync(destinationDir)) {
-          fs.mkdirSync(destinationDir, { recursive: true })
+          fs.rm(filePath, { recursive: true, force: true })
         }
+      } catch (err) {
+        console.error('Error clearing uploads directory:', err)
+        throw new Error('Error clearing uploads directory')
+      }
 
-        if (fs.existsSync(filePath)) {
-          fs.copyFileSync(filePath, destinationPath)
-          console.log(`Copied file: ${fileName} to ${destinationPath}`)
+      const extractedPath = path.join('uploads')
+
+      await fs
+        .createReadStream(templateFile.path)
+        .pipe(unzip.Extract({ path: extractedPath }))
+        .promise()
+      console.log('File extracted successfully to:', extractedPath)
+
+      fs.unlink(templateFile.path, (err) => {
+        if (err) {
+          console.error('Error deleting original zip file:', err)
         } else {
-          console.warn(`File not found: ${filePath}`)
+          console.log('Original zip file deleted successfully.')
         }
       })
 
-      res.status(200).json({ message: 'Folder uploaded successfully.' })
+      res.send('File uploaded and extracted successfully.')
     } catch (error) {
-      console.error('Error uploading folder:', error)
-      res.status(500).json({ message: 'Error uploading folder.' })
-    }
-  })
-
-  app.post('/upload-folder', upload.array('files'), (req, res) => {
-    try {
-      console.log('Files uploaded:', req.files)
-
-      res.status(200).json({ message: 'Priečinok úspešne nahratý.' })
-    } catch (error) {
-      console.error(error)
-      res.status(500).json({ message: 'Chyba pri nahrávaní priečinku.' })
-    }
-  })
-
-  app.get('/folder-files', async (req, res) => {
-    const folderPath = decodeURIComponent(req.query.path)
-    console.log(folderPath)
-
-    const getAllFiles = (dirPath, arrayOfFiles) => {
-      const files = fs.readdirSync(dirPath)
-
-      arrayOfFiles = arrayOfFiles || []
-
-      files.forEach((file) => {
-        const filePath = path.join(dirPath, file)
-
-        if (fs.statSync(filePath).isDirectory()) {
-          arrayOfFiles = getAllFiles(filePath, arrayOfFiles)
-        } else {
-          arrayOfFiles.push(filePath)
-        }
-      })
-
-      return arrayOfFiles
-    }
-
-    try {
-      const allFiles = getAllFiles(folderPath)
-      res.status(200).json({ files: allFiles })
-    } catch (error) {
-      console.error('Error reading folder:', error)
-      res.status(500).send('Error reading folder')
+      console.error('Error processing form data:', error)
+      res.status(500).send('Error processing form data')
     }
   })
 
@@ -140,11 +97,10 @@ export const render = (app) => {
         repeats: req.body.repeats,
         article: req.body.article,
         width: req.body.size.width,
-        height: req.body.size.height,
-        customer: req.body.customer
+        height: req.body.size.height
       }
-
       const imageBuffer = await PhantomRenderer.renderImage(opts)
+      console.log(imageBuffer)
       if (!Buffer.isBuffer(imageBuffer)) {
         throw new Error('imageBuffer is not a buffer object')
       }
