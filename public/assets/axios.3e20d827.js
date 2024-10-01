aa692ef34b2dbe39e44fbce06d57ccd4812a0d8e
Daniel Trstansky <daniel.trstansky@nov.sk>
2024-10-01 13:17:22 +0200

    nový build

---
+++ public/assets/axios.3e20d827.js
@@ -0,0 +1,1 @@
+import{b as i}from"./index.dac22646.js";import{a}from"./axios.bf56c3c5.js";const e=a.create({baseURL:"https://api.example.com"});var t=i(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=e});export{e as api,t as default};
